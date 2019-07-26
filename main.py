8bf1d1657e53a66b975d376bf9ba1a194d485f84
Ryan Hsu <stkoso0835@gmail.com>
2019-07-26 17:04:24 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Work with Python 3.6
+import dbl
+import discord
+from discord.ext import commands
+
+import asyncio
+import logging
+
+TOKEN = 'NjAzOTg2NDA4MzI3MzQ4MjU2.XTqeIA.UzuzfTbFaemqrVSUT_QJLtNqAqA'
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
