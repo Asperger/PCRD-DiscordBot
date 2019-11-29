46412617d2c15b3ef6e639475078fcbead7024e8
Ryan Hsu <stkoso0835@gmail.com>
2019-11-30 00:46:19 +0800

    Spam list limit. Sync protection

--- utils/spammer.py
+++ utils/spammer.py
@@ -1,9 +1,13 @@
 from collections import deque
 import os.path as path
+import threading
 from utils.log import FileLogger
 
 spam_setting_path = path.join(path.dirname(__file__), 'spam.setting')
 spam_setting = {}
+spam_lock = threading.Lock()
+spam_limit = 5
+
 if path.exists(spam_setting_path):
     with open(spam_setting_path, 'r') as f:
         spam_setting_str = f.read()
@@ -15,42 +19,53 @@ if path.exists(spam_setting_path):
                 spam_setting = {}
 
 def backup():
-    with open(spam_setting_path, 'w') as f:
-        try:
-            f.write(repr(spam_setting))
-        except Exception as e:
-            FileLogger.error(f'Fail to write spam setting: {str(e)}')
+    with spam_lock:
+        with open(spam_setting_path, 'w') as f:
+            try:
+                f.write(repr(spam_setting))
+            except Exception as e:
+                FileLogger.error(f'Fail to write spam setting: {str(e)}')
 
 def set_spammer(request, response):
     global spam_setting
-    if request in spam_setting:
-        spam_setting[request]["list"].append(response)
-    else:
-        spam_setting[request] = {"index": -1, "list": deque([response])}
+
+    with spam_lock:
+        if request in spam_setting:
+            spam_setting[request]["list"].append(response)
+            if len(spam_setting[request]["list"]) > spam_limit:
+                spam_setting[request]["list"].popleft()
+        else:
+            spam_setting[request] = {"index": -1, "list": deque([response])}
 
     backup()
     return True
 
 def clear_spammer(request, mode):
     global spam_setting
-    if request in spam_setting:
-        if mode == 0 or len(spam_setting[request]["list"]) == 1:
-            del spam_setting[request]
-        elif mode > 0:
-            spam_setting[request]["list"].popleft()
-        elif mode < 0:
-            spam_setting[request]["list"].pop()
+    result = False
 
+    with spam_lock:
+        if request in spam_setting:
+            if mode == 0 or len(spam_setting[request]["list"]) == 1:
+                del spam_setting[request]
+            elif mode > 0:
+                spam_setting[request]["list"].popleft()
+            elif mode < 0:
+                spam_setting[request]["list"].pop()
+            result = True
+
+    if result:
         backup()
-        return True
-    else:
-        return False
+    return result
 
 def get_spammer(request):
     global spam_setting
     result = None
-    if request in spam_setting:
-        index = (spam_setting[request]["index"] + 1) % len(spam_setting[request]["list"])
-        spam_setting[request]["index"] = index
-        result = spam_setting[request]["list"][index]
+
+    with spam_lock:
+        if request in spam_setting:
+            index = (spam_setting[request]["index"] + 1) % len(spam_setting[request]["list"])
+            spam_setting[request]["index"] = index
+            result = spam_setting[request]["list"][index]
+
     return result
