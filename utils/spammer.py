eca456c32572112e8fe67a4848e8c62317c880c5
Ryan Hsu <stkoso0835@gmail.com>
2019-11-29 21:13:36 +0800

    partial removal for spammer

--- utils/spammer.py
+++ utils/spammer.py
@@ -1,17 +1,47 @@
+from collections import deque
+import os.path as path
+from utils.log import FileLogger
+
+spam_setting_path = path.join(path.dirname(__file__), 'spam.setting')
 spam_setting = {}
+if path.exists(spam_setting_path):
+    with open(spam_setting_path, 'r') as f:
+        spam_setting_str = f.read()
+        if spam_setting_str:
+            try:
+                spam_setting = eval(spam_setting_str)
+            except Exception as e:
+                FileLogger.error(f'Fail to read spam setting: {str(e)}')
+                spam_setting = {}
+
+def backup():
+    with open(spam_setting_path, 'w') as f:
+        try:
+            f.write(repr(spam_setting))
+        except Exception as e:
+            FileLogger.error(f'Fail to write spam setting: {str(e)}')
 
 def set_spammer(request, response):
     global spam_setting
     if request in spam_setting:
         spam_setting[request]["list"].append(response)
     else:
-        spam_setting[request] = {"index": 0, "list": [response]}
+        spam_setting[request] = {"index": -1, "list": deque([response])}
+
+    backup()
     return True
 
-def clear_spammer(request):
+def clear_spammer(request, mode):
     global spam_setting
     if request in spam_setting:
-        del spam_setting[request]
+        if mode == 0 or len(spam_setting[request]["list"]) == 1:
+            del spam_setting[request]
+        elif mode > 0:
+            spam_setting[request]["list"].popleft()
+        elif mode < 0:
+            spam_setting[request]["list"].pop()
+
+        backup()
         return True
     else:
         return False
@@ -20,7 +50,7 @@ def get_spammer(request):
     global spam_setting
     result = None
     if request in spam_setting:
-        index = spam_setting[request]["index"]
+        index = (spam_setting[request]["index"] + 1) % len(spam_setting[request]["list"])
+        spam_setting[request]["index"] = index
         result = spam_setting[request]["list"][index]
-        spam_setting[request]["index"] = (index+1) % len(spam_setting[request]["list"])
     return result
