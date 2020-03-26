70d8305d09feeea1df5b01e310697882546b5d4e
Ryan Hsu <stkoso0835@gmail.com>
2020-03-26 15:32:16 +0800

    fix timezone issue of daily task

--- utils/timer.py
+++ utils/timer.py
@@ -5,6 +5,7 @@ import schedule
 import time
 import threading
 from utils.line_manager import clear_line
+from utils.log import FileLogger
 
 timer_usage = 0
 timer_member = 0
@@ -18,7 +19,10 @@ def get_settlement_time_object():
 def get_settlement_time():
     return datetime.now(_eu_moscow).strftime('%Y-%m-%d')
 
-schedule.every().day.at("05:00").do(clear_line, boss_id=0)
+_transition_point = get_settlement_time_object().replace(hour=0, minute=0, second=0, microsecond=0)
+_local_transition_point = _transition_point.astimezone()
+FileLogger.info(f"Schedule daily task clear_line at {_local_transition_point}")
+schedule.every().day.at(_local_transition_point.strftime('%H:%M:%S')).do(clear_line, boss_id=0)
 
 cease_continuous_run = threading.Event()
 class ScheduleThread(threading.Thread):
